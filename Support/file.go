061bfb5326586d7942070f2e74055ff4a78d20a6
agoalofalife <agoalofalife@gmail.com>
2017-07-14 14:24:57 +0300

    add command copy file

---
+++ Support/file.go
@@ -0,0 +1,28 @@
+package Support
+
+import (
+	"os"
+	"io"
+)
+
+
+// copy file
+func Cp(dst, src string) error {
+	s, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	// no need to check errors on read only file, we already got everything
+	// we need from the filesystem, so nothing can go wrong now.
+	defer s.Close()
+	d, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	if _, err := io.Copy(d, s); err != nil {
+		d.Close()
+		return err
+	}
+	return d.Close()
+}
+
--- commands/copyConfig.go
+++ commands/copyConfig.go
@@ -3,45 +3,26 @@ package commands
 import (
 	"github.com/urfave/cli"
 	"os"
-	"io"
 	"fmt"
 	"github.com/shiena/ansicolor"
+	"github.com/agoalofalife/reuse/Support"
 )
 
 const configName  = "example.config.json"
-const gopath = "$GOPATH"
+const goPath = "$GOPATH"
 
 func CopyConfig() cli.Command {
+	// copy config json from stubs to base dir $GOPATH
 	 return cli.Command{
 		 Name:  "copy-config",
 		 Usage: "Adds a copy of the configuration file",
 		 Action: func(c *cli.Context) error {
 			 dir, _ := os.Getwd()
 			 w := ansicolor.NewAnsiColorWriter(os.Stdout)
-			 text := "%sFile " + os.ExpandEnv(gopath) + "/" + "reuse.config.json " +"create success \n"
-			 fmt.Fprintf(w,text, "\x1b[32m")
-			 cp(os.ExpandEnv(gopath) + "/" + "reuse.config.json", dir + "/stubs/" + configName,)
-			 //fmt.Println("added task: ",dir + "/" + configName)
-			 //fmt.Println("added task: ", c.Args().First())
+			 text := "%sFile %s" + os.ExpandEnv(goPath) + "/" + "reuse.config.json " +"%screate success \n"
+			 fmt.Fprintf(w,text, "\x1b[32m", "\x1b[92m", "\x1b[32m")
+			 Support.Cp(os.ExpandEnv(goPath) + "/" + "reuse.config.json", dir + "/stubs/" + configName,)
 			 return nil
 		 }}
 }
 
-func cp(dst, src string) error {
-	s, err := os.Open(src)
-	if err != nil {
-		return err
-	}
-	// no need to check errors on read only file, we already got everything
-	// we need from the filesystem, so nothing can go wrong now.
-	defer s.Close()
-	d, err := os.Create(dst)
-	if err != nil {
-		return err
-	}
-	if _, err := io.Copy(d, s); err != nil {
-		d.Close()
-		return err
-	}
-	return d.Close()
-}
