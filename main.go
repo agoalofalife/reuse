4f22e774011e6deaa6dec089df7c8a28a6cb7156
agoalofalife <agoalofalife@gmail.com>
2017-07-14 11:58:19 +0300

    add cli and author

--- main.go
+++ main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"github.com/urfave/cli"
+)
 
 func main(){
-	fmt.Print("Hello from funciton!")
+
+
+	app := cli.NewApp()
+	// EXAMPLE: Append to an existing template
+	cli.AppHelpTemplate = fmt.Sprintf(`%s
+
+WEBSITE: http://awesometown.example.com
+
+SUPPORT: support@awesometown.example.com
+
+`, cli.AppHelpTemplate)
+
+	// EXAMPLE: Override a template
+	cli.AppHelpTemplate = `NAME:
+   {{.Name}} - {{.Usage}}
+USAGE:
+   {{.HelpName}} {{if .VisibleFlags}}[global options]{{end}}{{if .Commands}} command [command options]{{end}} {{if .ArgsUsage}}{{.ArgsUsage}}{{else}}[arguments...]{{end}}
+   {{if len .Authors}}
+AUTHOR:
+   {{range .Authors}}{{ . }}{{end}}
+   {{end}}{{if .Commands}}
+COMMANDS:
+{{range .Commands}}{{if not .HideHelp}}   {{join .Names ", "}}{{ "\t"}}{{.Usage}}{{ "\n" }}{{end}}{{end}}{{end}}{{if .VisibleFlags}}
+GLOBAL OPTIONS:
+   {{range .VisibleFlags}}{{.}}
+   {{end}}{{end}}{{if .Copyright }}
+COPYRIGHT:
+   {{.Copyright}}
+   {{end}}{{if .Version}}
+VERSION:
+   {{.Version}}
+   {{end}}
+`
+
+	// EXAMPLE: Replace the `HelpPrinter` func
+	//cli.HelpPrinter = func(w io.Writer, templ string, data interface{}) {
+	//	fmt.Println("Ha HA.  I pwnd the help!!1")
+	//}
+	app.Name = "reuse"
+	app.Authors = []cli.Author{cli.Author{"Ilya Chubarov", "agoalofalife@gmail.com"}}
+	app.Run(os.Args)
 }
