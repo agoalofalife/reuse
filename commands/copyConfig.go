eeb2a2b832ed82c5fa3ca0dd227c9111af7a3079
agoalofalife <agoalofalife@gmail.com>
2017-07-14 12:56:51 +0300

    add folder stubs

--- commands/copyConfig.go
+++ commands/copyConfig.go
@@ -1,3 +1,45 @@
 package commands
 
+import (
+	"github.com/urfave/cli"
+	"os"
+	"io"
+	"fmt"
+)
 
+const configName  = "example.config.json"
+const gopath = "$GOPATH"
+
+func CopyConfig() cli.Command {
+	 return cli.Command{
+		 Name:  "copy-config",
+		 Usage: "Adds a copy of the configuration file",
+		 Action: func(c *cli.Context) error {
+			 dir, _ := os.Getwd()
+
+			 fmt.Println("added task: ", dir + "/stubs/" + configName)
+			 cp(os.ExpandEnv(gopath) + "/" + "reuse.config.json", dir + "/stubs/" + configName,)
+			 //fmt.Println("added task: ",dir + "/" + configName)
+			 //fmt.Println("added task: ", c.Args().First())
+			 return nil
+		 }}
+}
+
+func cp(dst, src string) error {
+	s, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	// no need to check errors on read only file, we already got everything
+	// we need from the filesystem, so nothing can go wrong now.
+	defer s.Close()
+	d, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	if _, err := io.Copy(d, s); err != nil {
+		d.Close()
+		return err
+	}
+	return d.Close()
+}
--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"github.com/agoalofalife/reuse/commands"
 	"github.com/urfave/cli"
+	"os"
 	"time"
 )
 
@@ -10,13 +12,14 @@ type manager struct {
 	command []cli.Command
 }
 
-func (manager *manager) add(command cli.Command)  {
+func (manager *manager) add(command cli.Command) {
 	manager.command = append(manager.command, command)
 }
 
-func main(){
+func main() {
 	app := cli.NewApp()
 	manager := manager{}
+	manager.add(commands.CopyConfig())
 
 	cli.VersionPrinter = func(c *cli.Context) {
 		fmt.Fprintf(c.App.Writer, "version=%s\n", c.App.Version)
@@ -24,13 +27,12 @@ func main(){
 	app.Name = "reuse"
 	app.Authors = []cli.Author{
 		cli.Author{
-			Name: "Ilya Chubarov",
+			Name:  "Ilya Chubarov",
 			Email: "agoalofalife@gmail.com",
 		},
 	}
 	app.Compiled = time.Now()
 	app.Usage = "Cli application"
-
 
 	app.Flags = []cli.Flag{
 		cli.BoolFlag{Name: "fancy"},
@@ -39,37 +41,37 @@ func main(){
 	}
 
 	app.Commands = manager.command
-		//[]cli.Command{
-		//{
-		//	Name:    "generate-config",
-		//	Usage:   "Adds a copy of the configuration file",
-		//	Action:  func(c *cli.Context) error {
-		//		fmt.Println("added task: ", c.Args().First())
-		//		return nil
-		//	},
-		//},
-		//{
-		//	Name:        "template",
-		//	Usage:       "options for task templates",
-		//	Subcommands: []cli.Command{
-		//		{
-		//			Name:  "add",
-		//			Usage: "add a new template",
-		//			Action: func(c *cli.Context) error {
-		//				fmt.Println("new task template: ", c.Args().First())
-		//				return nil
-		//			},
-		//		},
-		//		{
-		//			Name:  "remove",
-		//			Usage: "remove an existing template",
-		//			Action: func(c *cli.Context) error {
-		//				fmt.Println("removed task template: ", c.Args().First())
-		//				return nil
-		//			},
-		//		},
-		//	},
-		//},
+	//[]cli.Command{
+	//{
+	//	Name:    "generate-config",
+	//	Usage:   "Adds a copy of the configuration file",
+	//	Action:  func(c *cli.Context) error {
+	//		fmt.Println("added task: ", c.Args().First())
+	//		return nil
+	//	},
+	//},
+	//{
+	//	Name:        "template",
+	//	Usage:       "options for task templates",
+	//	Subcommands: []cli.Command{
+	//		{
+	//			Name:  "add",
+	//			Usage: "add a new template",
+	//			Action: func(c *cli.Context) error {
+	//				fmt.Println("new task template: ", c.Args().First())
+	//				return nil
+	//			},
+	//		},
+	//		{
+	//			Name:  "remove",
+	//			Usage: "remove an existing template",
+	//			Action: func(c *cli.Context) error {
+	//				fmt.Println("removed task template: ", c.Args().First())
+	//				return nil
+	//			},
+	//		},
+	//	},
+	//},
 	//}
-	//app.Run(os.Args)
+	app.Run(os.Args)
 }
