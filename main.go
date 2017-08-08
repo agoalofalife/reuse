288e1b493793d8bd88656ceefc27403e2864a9de
agoalofalife <agoalofalife@gmail.com>
2017-08-08 17:11:32 +0300

    add store container

--- main.go
+++ main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"fmt"
 	"github.com/agoalofalife/reuse/commands"
+	store "github.com/agoalofalife/storekeeper"
 	"github.com/urfave/cli"
 	"os"
 	"time"
 )
+
+type Application struct {
+	Container *store.Store
+}
 
 type manager struct {
 	command []cli.Command
@@ -17,7 +22,11 @@ func (manager *manager) add(command cli.Command) {
 }
 
 func main() {
-	app := cli.NewApp()
+	app := Application{store.New()}
+	app.Container.SetInstance(`command`, cli.NewApp())
+
+	appCommand := app.Container.Extract(`command`).(*cli.App)
+
 	manager := manager{}
 	manager.add(commands.CopyConfig())
 	manager.add(commands.StartServer())
@@ -25,20 +34,20 @@ func main() {
 	cli.VersionPrinter = func(c *cli.Context) {
 		fmt.Fprintf(c.App.Writer, "version=%s\n", c.App.Version)
 	}
-	app.Name = "reuse"
-	app.Authors = []cli.Author{
+	appCommand.Name = "reuse"
+	appCommand.Authors = []cli.Author{
 		cli.Author{
 			Name:  "Ilya Chubarov",
 			Email: "agoalofalife@gmail.com",
 		},
 	}
-	app.Compiled = time.Now()
-	app.Usage = "Cli application"
+	appCommand.Compiled = time.Now()
+	appCommand.Usage = "Cli application"
 
-	app.Flags = []cli.Flag{
+	appCommand.Flags = []cli.Flag{
 		cli.DurationFlag{Name: "howlong, H", Value: time.Second * 3},
 	}
 
-	app.Commands = manager.command
-	app.Run(os.Args)
+	appCommand.Commands = manager.command
+	appCommand.Run(os.Args)
 }
