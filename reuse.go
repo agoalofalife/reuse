7ed49ba5becd0cdf983cb138346081f068c90f00
agoalofalife <agoalofalife@gmail.com>
2017-08-09 17:56:50 +0300

    try set valid path configuration

--- reuse.go
+++ reuse.go
@@ -1,11 +1,14 @@
 package reuse
 
 import (
+	"fmt"
 	"github.com/agoalofalife/reuse/log"
 	store "github.com/agoalofalife/storekeeper"
 	"github.com/astaxie/beego/config"
 	"github.com/gorilla/mux"
+	"os"
 	"path"
+	"path/filepath"
 	"runtime"
 )
 
@@ -27,18 +30,38 @@ type Application struct {
 
 // so start App..
 func Run() {
-	loadDependencies()
 	app.Container.Extract(`server`).(*Server).Run()
 }
 
-func loadDependencies() {
+func Bootstrapping() {
 	app = Application{store.New()}
 
+	//fmt.Println(filepath.Abs(filepath.Dir(os.Args[0])))
+	//workPath,_ :=  os.Getwd()
+	fmt.Println(os.Getwd())
+	AppPath, _ := filepath.Abs(filepath.Dir(os.Args[0]))
+
+	//appConfigPath := filepath.Join(workPath, "conf", "app.conf")
+	//fmt.Println(appConfigPath)
+	appConfigPath := filepath.Join(AppPath, "conf", "app.conf")
+	fmt.Println(appConfigPath)
+	os.Exit(0)
+
+	//if !utils.FileExists(appConfigPath) {
+	//	appConfigPath = filepath.Join(AppPath, "conf", "app.conf")
+	//	//if !utils.FileExists(appConfigPath) {
+	//	//	AppConfig = &beegoAppConfig{innerConfig: config.NewFakeConfig()}
+	//	//	return
+	//	//}
+	//}
+	//if err = parseConfig(appConfigPath); err != nil {
+	//	panic(err)
+	//}
 	// TODO add the ability change path configuration for remote
 	_, filename, _, _ := runtime.Caller(1)
 	fileConf := path.Join(path.Dir(filename), relativePathConf)
 	configuration, errConfig := config.NewConfig(typeConf, fileConf)
-
+	fmt.Println(filename, `???a`)
 	server, err := NewServer(map[string]string{"port": configuration.String(`port`),
 		"staticPath": configuration.String(`staticPath`),
 		"staticUrl":  configuration.String(`staticUrl`)})
