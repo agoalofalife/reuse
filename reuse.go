a3352228a8c7f25acc015588b845d734beb659bf
agoalofalife <agoalofalife@gmail.com>
2017-08-09 10:40:57 +0300

    clean code

--- reuse.go
+++ reuse.go
@@ -4,7 +4,6 @@ import (
 	"github.com/agoalofalife/reuse/log"
 	store "github.com/agoalofalife/storekeeper"
 	"github.com/gorilla/mux"
-	"github.com/urfave/cli"
 )
 
 var app Application
@@ -13,48 +12,10 @@ type Application struct {
 	Container *store.Store
 }
 
-type manager struct {
-	command []cli.Command
-}
-
-func (manager *manager) add(command cli.Command) {
-	manager.command = append(manager.command, command)
-}
-
 func Run() {
 	app = Application{store.New()}
 	loadDependencies()
 	app.Container.Extract(`server`).(*Server).Run()
-	//
-	//app.Container.Bind(`core`, func(s *store.Store) *core.Server {
-	//	return core.NewServer(os.ExpandEnv(goPath) + "/" + "reuse.config.json")
-	//})
-	//
-	//appCommand := app.Container.Extract(`command`).(*cli.App)
-	//
-	//manager := manager{}
-	//manager.add(commands.CopyConfig())
-	//manager.add(commands.StartServer())
-	//
-	//cli.VersionPrinter = func(c *cli.Context) {
-	//	fmt.Fprintf(c.App.Writer, "version=%s\n", c.App.Version)
-	//}
-	//appCommand.Name = "reuse"
-	//appCommand.Authors = []cli.Author{
-	//	cli.Author{
-	//		Name:  "Ilya Chubarov",
-	//		Email: "agoalofalife@gmail.com",
-	//	},
-	//}
-	//appCommand.Compiled = time.Now()
-	//appCommand.Usage = "Cli application"
-	//
-	//appCommand.Flags = []cli.Flag{
-	//	cli.DurationFlag{Name: "howlong, H", Value: time.Second * 3},
-	//}
-	//
-	//appCommand.Commands = manager.command
-	//appCommand.Run(os.Args)
 }
 
 func loadDependencies() {
@@ -65,7 +26,6 @@ func loadDependencies() {
 	}
 
 	r := mux.NewRouter()
-
 	app.Container.SetInstance(`server`, server)
 	app.Container.SetInstance(`router`, r)
 	app.Container.SetInstance(`log`, log.NewLog())
