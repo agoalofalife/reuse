177fa5d30fb22cbe86b6acff9ddfaad4649bcd1e
agoalofalife <agoalofalife@gmail.com>
2017-08-10 11:26:21 +0300

    configuration builder

--- config.go
+++ config.go
@@ -2,10 +2,25 @@ package reuse
 
 import "github.com/astaxie/beego/config"
 
+const (
+	relativePathConf = `/config/app.conf`
+	typeConf         = `ini`
+)
+
 type Config struct {
 	// path to file
 	pathToFile string
 	// responsible
 	responsible config.Configer
+}
 
+// create new Structure Config
+func NewConfig(configPath string) *Config {
+	configuration, errConfig := config.NewConfig(typeConf, configPath)
+	if errConfig != nil {
+		panic(errConfig.Error())
+	}
+	pointerConfig := &Config{configPath, configuration}
+	app.Container.SetInstance(`config`, pointerConfig)
+	return pointerConfig
 }
--- reuse.go
+++ reuse.go
@@ -1,7 +1,6 @@
 package reuse
 
 import (
-	"fmt"
 	"github.com/agoalofalife/reuse/log"
 	"github.com/agoalofalife/reuse/supports/files"
 	store "github.com/agoalofalife/storekeeper"
@@ -13,20 +12,11 @@ import (
 	"runtime"
 )
 
-const (
-	relativePathConf = `/config/app.conf`
-	typeConf         = `ini`
-)
-
 var app Application
 
 type Application struct {
 	// container store struct in application
 	Container *store.Store
-	// configuration module from beego
-	//Config config.Configer
-	//// default router from gorilla mux
-	//Router *mux.Router
 }
 
 // so start App..
@@ -38,23 +28,16 @@ func Run() {
 func bootstrapping() {
 	app = kernel()
 
+	//AppPath, _ := filepath.Abs(filepath.Dir(os.Args[0]))
+	//appConfigPath = filepath.Join(AppPath, "conf", "app.conf")
 	workDir, _ := os.Getwd()
-	AppPath, _ := filepath.Abs(filepath.Dir(os.Args[0]))
 	appConfigPath := filepath.Join(workDir, "conf", "app.conf")
-	//fmt.Println(appConfigPath)
-	//appConfigPath := filepath.Join(AppPath, "conf", "app.conf")
 
-	fmt.Println(files.FileExists(appConfigPath), `?12`)
 	if !files.FileExists(appConfigPath) {
-		appConfigPath = filepath.Join(AppPath, "conf", "app.conf")
-		if !files.FileExists(appConfigPath) {
-			//AppConfig = &beegoAppConfig{innerConfig: config.NewFakeConfig()}
-			return
-		}
+		panic(`File configuration is not set!`)
 	}
-	//if err = parseConfig(appConfigPath); err != nil {
-	//	panic(err)
-	//}
+
+	NewConfig(appConfigPath)
 
 	// TODO add the ability change path configuration for remote
 	_, filename, _, _ := runtime.Caller(1)
